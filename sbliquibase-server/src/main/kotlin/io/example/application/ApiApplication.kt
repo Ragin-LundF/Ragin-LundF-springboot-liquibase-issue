72baa7b66d136bb0f6c8f131a7d49d117d87c32f
Jörg Flade <joerg.flade@finapi.io>
2024-08-28 12:56:59 +0200

    Simplification

--- sbliquibase-server/src/main/kotlin/io/example/application/ApiApplication.kt
+++ sbliquibase-server/src/main/kotlin/io/example/application/ApiApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableAsync
@@ -20,7 +19,6 @@ import org.springframework.scheduling.annotation.EnableAsync
 @EnableAsync
 @EntityScan(basePackages = ["io.example.*"])
 @ComponentScan(basePackages = ["io.example.*"])
-@EnableFeignClients(basePackages = ["io.example.*"])
 @EnableJpaRepositories(basePackages = ["io.example.*"])
 @ConfigurationPropertiesScan(basePackages = [
     "io.example.*",
